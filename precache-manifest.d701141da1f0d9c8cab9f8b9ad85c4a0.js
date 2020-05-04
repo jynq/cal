d3019fa055e2bad715c79dafb64c48c103a15170
Jyn <jyn@o2.pl>
2020-05-04 15:17:54 +0200

    Updates

---
+++ precache-manifest.d701141da1f0d9c8cab9f8b9ad85c4a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447d822da221a19a81c18f23a4712cc2",
+    "url": "/cal/index.html"
+  },
+  {
+    "revision": "fd1d2428725f3682ca45",
+    "url": "/cal/static/css/main.3c04decc.chunk.css"
+  },
+  {
+    "revision": "28f3c810c3d631377380",
+    "url": "/cal/static/js/2.5040ed3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cal/static/js/2.5040ed3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1d2428725f3682ca45",
+    "url": "/cal/static/js/main.066ccd08.chunk.js"
+  },
+  {
+    "revision": "c63738e2771ada85b812",
+    "url": "/cal/static/js/runtime-main.19121328.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cal/static/media/logo.5d5d9eef.svg"
+  }
+]);
